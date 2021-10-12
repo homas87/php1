8ccd776d961914982eb02c44fba17203bd3d7cb2
homas87 <homas@naver.com>
2021-10-12 23:59:03 +0900

    페이지네이션 구현

--- _blog1page.php
+++ _blog1page.php
@@ -1,10 +1,32 @@
 <?php
 // 페이지네이션 
-// 조회
+
+if(isset($_GET['current_page'])){
+    $current_page = $_GET['current_page'];
+}else{
+    $current_page =1 ;
+}
+
 require('_conn.php');
-$sql = "SELECT * FROM `blog1` WHERE `del_flg`=0";
+
+
+//마지막 페이지 구하기
+
+$show_item = 3;
+$start_item = ($current_page - 1) * $show_item;
+
+$sql ="SELECT * FROM `blog1` WHERE 'del_flg' =0 ";
+$count = mysqli_query($conn, $sql) ->num_rows;
+$end_page = ceil($count/3);
+
+
+// 조회
+$sql = "SELECT * FROM `blog1` WHERE `del_flg`=0 ORDER BY no DESC LIMIT $start_item, $show_item" ;
 $result = mysqli_query($conn, $sql);
 
+//이전,다음 페이지
+$prev_page = $current_page - 1 ;
+$next_page = $current_page + 1 ;
 
 
 ?>
--- _login.php
+++ _login.php
@@ -11,7 +11,7 @@ $row = $result->num_rows; //결과값을 정수로 변환 (결과값이 1개면 
 if($row >0) {           //그러니까~ DB에서 user 테이블에 아이디와 비밀번호를 확인후 같으면 1이 나오니 1보다 크면 로그인처리 ㅇㅋ!)
     //로그인처리
     require('_loginok.php');
-    echo "<script>alert('로그인 되었습니다.!');location.href='index.php'</script>";
+    echo "<script>alert('로그인 되었습니다.');location.href='index.php'</script>";
 }else{
     echo "<script>alert('로그인 실패! 아이디, 비밀번호를 확인해주세요');location.href='login.php'</script>";
 }
